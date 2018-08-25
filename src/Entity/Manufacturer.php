1a22e61585ea8d1712f712469a94c35296a675b3
vagrant <vagrant@homestead>
2018-08-25 12:09:19 +0000

    Manufacturer entity class added

---
+++ src/Entity/Manufacturer.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ManufacturerRepository")
+ * @UniqueEntity("slug"),
+ *     errorPath="slug",
+ *     message="This slug is already in use."
+ */
+class Manufacturer
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=false, unique=true)
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * Image path to logo
+     *
+     * @var string
+     *
+     * @ORM\Column(type="text", length=255, nullable=true)
+     */
+    protected $logo;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getLogo(): string
+    {
+        return $this->logo;
+    }
+
+
+    public function setLogo(string $logo): ?string
+    {
+        $this->logo = $logo;
+
+        return $this;
+    }
+}
